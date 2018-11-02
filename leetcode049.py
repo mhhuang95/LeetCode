65e40943907fa9447ce13438a1c68ce1557f5860
mhhuang95 <mhhuang@ucdavis.edu>
2018-11-02 10:41:00 -0700

    Group Anagrams

--- leetcode049.py
+++ leetcode049.py
@@ -20,6 +20,7 @@ class Solution:
             table[ord(x) - ord('a')] += 1
         return str(table)
 
+
 if __name__ == "__main__":
     s = Solution()
     print(s.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
