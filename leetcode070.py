27715ce0004d7b7b89d1cbd3e319aefbb7d0c564
mhhuang95 <mhhuang@ucdavis.edu>
2018-11-04 22:03:45 -0800

    Climbing Stairs

---
+++ leetcode070.py
@@ -0,0 +1,15 @@
+class Solution:
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        dp = [1 for i in range(n)]
+        dp[1] = 2
+        for i in range(2, n):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
