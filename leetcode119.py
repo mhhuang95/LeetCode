79478c2c87e6d52491834b51fb35913ee17cf02d
mhhuang95 <mhhuang@ucdavis.edu>
2018-12-31 08:59:53 -0800

    class Solution:     def getRow(self, rowIndex):

---
+++ leetcode119.py
@@ -0,0 +1,18 @@
+class Solution:
+    def getRow(self, rowIndex):
+        """
+        :type rowIndex: int
+        :rtype: List[int]
+        """
+        tria = [1]
+        for i in range(rowIndex-1):
+            x = [1]
+            for j in range(len(tria)-1):
+                x.append(tria[j]+tria[j+1])
+            x.append(1)
+            tria = x
+        return tria
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.getRow(3))
