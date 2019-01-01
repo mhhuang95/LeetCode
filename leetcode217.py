d0a13695016daad5363947bc5683469bce5f4fbf
mhhuang95 <mhhuang@ucdavis.edu>
2018-12-31 22:31:35 -0800

    class Solution(object):     def containsDuplicate(self, nums):

---
+++ leetcode217.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dic = {}
+        for x in nums:
+            if x in dic:
+                return True
+            else:
+                dic[x] = 1
+        return False
