25621330574868a3d3b4eaaa4dd9321d6938edb2
mhhuang95 <mhhuang@ucdavis.edu>
2019-01-04 22:36:06 -0800

    Count Primes

---
+++ leetcode204.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def countPrimes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n < 3:
+            return 0
+        prime = [1]*(n)
+        prime[0] = prime[1] = 0
+        for i in range(2, int(n**0.5)+1):
+            if prime[i]:
+                prime[i*i:n:i] = [0]*len(prime[i*i:n:i])
+        return sum(prime)
