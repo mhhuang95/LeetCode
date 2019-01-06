42d59697fa0b76ac00606c51f5d50b8c7b4b861c
mhhuang95 <mhhuang@ucdavis.edu>
2019-01-05 20:32:36 -0800

    Binary Tree Postorder Traversal

--- leetcode145.py
+++ leetcode145.py
@@ -14,3 +14,15 @@ class Solution(object):
         if not root:
             return []
         return self.postorderTraversal(root.left) + self.postorderTraversal(root.right) + [root.val]
+
+    class Solution(object):
+        def postorderTraversal(self, root):
+            stack, res = [root], []
+            while stack:
+                if stack[-1] != None and (stack[-1].left != None or stack[-1].right != None):
+                    stack.append(stack[-1].right)
+                    stack.append(stack[-1].left)
+                node = stack.pop()
+                if node != None:
+                    res.append(node.val)
+            return res
