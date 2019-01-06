8f102aeac245d5fcbabca7ec13ef5c4f71584480
mhhuang95 <mhhuang@ucdavis.edu>
2019-01-06 11:28:19 -0800

    Verify Preorder Sequence in Binary Search Tree

---
+++ leetcode255.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def verifyPreorder(self, preorder):
+        """
+        :type preorder: List[int]
+        :rtype: bool
+        """
+        stack = []
+        lower = -1<<31
+        for x in preorder:
+            if x < lower:
+                return False
+            while stack and x > stack[-1]:
+                lower = stack.pop()
+            stack.append(x)
+        return True
+
+
+if __name__ == "__main__" :
+    s = Solution()
+    print(s.verifyPreorder([5,2,6,1,3]))
