65474e91c62311ef984b5d9b1391843f2290ae1f
mhhuang95 <mhhuang@ucdavis.edu>
2019-01-05 21:49:53 -0800

    Populating Next Right Pointers in Each Node

---
+++ leetcode116.py
@@ -0,0 +1,25 @@
+# Definition for binary tree with next pointer.
+# class TreeLinkNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+#         self.next = None
+
+class Solution:
+    # @param root, a tree link node
+    # @return nothing
+    def connect(self, root):
+        if not root:
+            return
+        curr = root
+        n = curr.left
+
+        while curr.left:
+            curr.left.next = curr.right
+            if curr.next:
+                curr.right.next = curr.next.left
+                curr = curr.next
+            else:
+                curr = n
+                n = curr.left
