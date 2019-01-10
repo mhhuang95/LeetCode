ab7478e55f4530093b7cd4a884059aea76e901d6
mhhuang95 <mhhuang@ucdavis.edu>
2019-01-09 21:52:06 -0800

    Copy List with Random Pointer

---
+++ leetcode138.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list with a random pointer.
+# class RandomListNode(object):
+#     def __init__(self, x):
+#         self.label = x
+#         self.next = None
+#         self.random = None
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        m = n = head
+        dic = {}
+        while m:
+            dic[m] = RandomListNode(m.label)
+            m = m.next
+        while n:
+            dic[n].next = dic.get(n.next)
+            dic[n].random = dic.get(n.random)
+            n = n.next
+        return dic.get(head)
