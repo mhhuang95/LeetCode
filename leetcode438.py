1728cbc158ce80d656e1f6cce14e46b2a321d9d0
mhhuang95 <mhhuang@ucdavis.edu>
2019-01-10 16:56:01 -0800

    Find All Anagrams in a String

---
+++ leetcode438.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def findAnagrams(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: List[int]
+        """
+        res = []
+        n,m = len(s), len(p)
+        phash, shash = [0]*123, [0]*123
+        for x in p:
+            phash[ord(x)] += 1
+        for x in s[:m-1]:
+            shash[ord(x)] += 1
+        for i in range(m-1, n):
+            shash[ord(s[i])] += 1
+            if i-m >= 0:
+                shash[ord(s[i-m])] -= 1
+            if shash == phash:
+                res.append(i-m+1)
+        return res
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.findAnagrams("abab","ab"))
