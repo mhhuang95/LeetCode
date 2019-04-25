cec81f2210c5b4bfe9b35df5b2e00b3dbfa339d7
mhhuang95 <mhhuang@ucdavis.edu>
2019-04-25 10:03:34 -0700

    Reverse Integer

--- leetcodec++/leetcode007.cpp
+++ leetcodec++/leetcode007.cpp
@@ -1,5 +1,25 @@
 //
 // Created by 黄敏辉 on 2019-04-25.
 //
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
 
 
+class Solution {
+public:
+    int reverse(int x) {
+        long int res = 0;
+
+        while (x != 0){
+            res = res * 10 + x%10;
+            x = x / 10;
+            if(res > INT_MAX)
+                return 0;
+            if(res <INT_MIN)
+                return 0;
+        }
+        return res;
+    }
+};
