2778d9a6dfc6fe37458ae06488e3fe634f00b0aa
mhhuang95 <mhhuang@ucdavis.edu>
2019-05-09 15:42:28 -0700

    delete node in a link list

---
+++ leetcodec++/leetcode237.cpp
@@ -0,0 +1,20 @@
+//
+// Created by 黄敏辉 on 2019-05-09.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        auto next = node->next;
+        *node = *next;
+        delete next;
+    }
+};
