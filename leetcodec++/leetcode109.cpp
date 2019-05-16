6e6633a57aa28537e829e321ff0d7434f9128247
mhhuang95 <mhhuang@ucdavis.edu>
2019-05-15 22:43:18 -0700

    Convert Sorted List to Binary Search Tree

---
+++ leetcodec++/leetcode109.cpp
@@ -0,0 +1,52 @@
+//
+// Created by 黄敏辉 on 2019-05-15.
+//
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+    static int length(const ListNode* node){
+        int res = 0;
+        while(node){
+            res ++;
+            node = node->next;
+        }
+        return res;
+    }
+
+    TreeNode* create(ListNode*& cur, int count){
+        const int count_l = count/2;
+        const int count_r = count - count_l - 1;
+        auto node = new TreeNode(0);
+        if (count_l > 0)
+            node->left = create(cur, count_l);
+        node->val = cur->val;
+        cur = cur->next;
+        if (count_r > 0)
+            node->right = create(cur, count_r);
+        return node;
+    }
+
+
+public:
+    TreeNode* sortedListToBST(ListNode* head) {
+        const int len = length(head);
+        return (len > 0) ? create(head, len) : nullptr;
+
+    }
+};
