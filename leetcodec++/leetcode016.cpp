f4aa9a520d6425595987eda5dece066f753bba40
mhhuang95 <mhhuang@ucdavis.edu>
2019-06-27 22:11:08 +0800

    3 sum closest

---
+++ leetcodec++/leetcode016.cpp
@@ -0,0 +1,29 @@
+//
+// Created by 黄敏辉 on 2019-06-27.
+//
+
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int res = INT_MAX;
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size()-2; i++){
+            int l = i+1, r = nums.size() - 1;
+            while(l < r){
+                if (nums[i] + nums[l] + nums[r] == target){
+                    res = target;
+                    return res;
+                } else if (nums[i] + nums[l] + nums[r] > target){
+                    if (nums[i] + nums[l] + nums[r] - target < abs(res - target))
+                        res = nums[i] + nums[l] + nums[r];
+                    r--;
+                } else{
+                    if (target - (nums[i] + nums[l] + nums[r]) < abs(res - target))
+                        res = nums[i] + nums[l] + nums[r];
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+};
