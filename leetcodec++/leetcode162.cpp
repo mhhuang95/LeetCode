83df4ce005f7bf6262997ba4a2b120e9101bcde4
mhhuang95 <mhhuang@ucdavis.edu>
2019-08-25 15:47:10 +0800

    Find Peak Element:

--- leetcodec++/leetcode162.cpp
+++ leetcodec++/leetcode162.cpp
@@ -12,7 +12,18 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        if (    )
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while(low < high - 1){
+            int mid = (low + high) / 2;
+            if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1])
+                return mid;
+            else if (nums[mid] > nums[mid + 1])
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+
+        return nums[low] > nums[high] ? low : high;
     }
 };
