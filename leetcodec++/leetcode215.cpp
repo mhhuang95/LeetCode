c3e44821f2d832160ca7dbd0dbc60daa80cfe8a5
mhhuang95 <mhhuang@ucdavis.edu>
2019-09-02 17:36:51 +0800

    Kth Largest Element in an Array

---
+++ leetcodec++/leetcode215.cpp
@@ -0,0 +1,16 @@
+//
+// Created by 黄敏辉 on 2019-09-02.
+//
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        make_heap(nums.begin(), nums.end());
+        for(auto i=0; i<k-1;i++)
+        {
+            pop_heap(nums.begin(), nums.end());
+            nums.pop_back();
+        }
+        return nums.front();
+    }
+};
