a18513eff219ebb1e57d8edfaf68ef1fbcab6ee3
mhhuang95 <mhhuang@ucdavis.edu>
2019-09-06 23:39:29 +0800

    Basic Calculator II

--- leetcodec++/leetcode227.cpp
+++ leetcodec++/leetcode227.cpp
@@ -13,18 +13,37 @@
  */
 class Solution {
 public:
-    TreeNode* invertTree(TreeNode* root) {
-        if (!root)
-            return NULL;
-        TreeNode* tmp;
-
-        tmp = root->left;
-        root->left = root->right;
-        root->right = tmp;
-
-        root->left = invertTree(root->left);
-        root->right = invertTree(root->right);
-
-        return root;
+    int calculate(string s) {
+        stack<int> nums;
+        int res = 0;
+        int num = 0;
+        char sign = '+';
+        for(int i = 0; i < s.size(); i++){
+            if (isdigit(s[i])){
+                num = num * 10 + s[i] - '0';
+            }
+            if (!isdigit(s[i]) && !isspace(s[i]) || i == s.size() - 1){
+                if (sign == '-')
+                    nums.push(-num);
+                else if (sign == '+')
+                    nums.push(num);
+                else{
+                    int tmp;
+                    if (sign == '*')
+                        tmp = nums.top() * num;
+                    else
+                        tmp = nums.top() / num;
+                    nums.pop();
+                    nums.push(tmp);
+                }
+                sign = s[i];
+                num = 0;
+            }
+        }
+        while(!empty(nums)){
+            res += nums.top();
+            nums.pop();
+        }
+        return res;
     }
 };
